07cb64a94ad26b7a559b80156216f33efdecf50c
Junaid Ahmed <junaid.ahmed@postnord.com>
2023-06-24 08:53:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kafka_stream_spring_boot_3.x
---
+++ src/test/java/learning/kafka/spring/boot/stream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.kafka.spring.boot.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
